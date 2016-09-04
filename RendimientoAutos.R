361a1b92a62b8a2a12895237858662ae3765539f
CRUR <estadistica.aplicada.itam@gmail.com>
2016-09-04 17:49:39 -0500

    Se agrego la relacion de hwy con displ

--- RendimientoAutos.R
+++ RendimientoAutos.R
@@ -4,3 +4,11 @@ cat("Primeras observaciones:\n"); head(mpg)
 cat("Estructura de los datos:\n"); str(mpg)
 cat("Estadisticas descriptivas:\n"); summary(mpg)
 cat("Numero de valores faltantes:\n"); sum(is.na(mpg))
+# Relacion entre millas por galon (autopista) y el tamanio del motor.
+ggplot(mpg, aes(displ, hwy, colour = drv)) +
+  geom_point() +
+  labs(title = "Relacion numero de millas por galon (autopista) vs tamanio del motor",
+       x = "Tamanio del motor",
+       y = "Numero de millas recorridas por galon en carretera",
+       colour = "Traccion") +
+  theme_bw()
