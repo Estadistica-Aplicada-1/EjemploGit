bdcb68ec3a2fe7430f7ea5caad7180a8be35a629
CRUR <estadistica.aplicada.itam@gmail.com>
2016-09-04 17:53:29 -0500

    Definicion de Funciones

--- RendimientoAutos.R
+++ RendimientoAutos.R
@@ -1,9 +1,6 @@
 library(ggplot2)
 # Utilizamos el data frame mpg (?mpg) y obtenemos informacion sobre dicho objeto.
-cat("Primeras observaciones:\n"); head(mpg)
-cat("Estructura de los datos:\n"); str(mpg)
-cat("Estadisticas descriptivas:\n"); summary(mpg)
-cat("Numero de valores faltantes:\n"); sum(is.na(mpg))
+source("./Funciones/ObtenerInformacion.R"); GenerarResumen(mpg)
 # Relacion entre millas por galon (autopista) y el tamanio del motor.
 ggplot(mpg, aes(displ, hwy, colour = drv)) +
   geom_point() +
