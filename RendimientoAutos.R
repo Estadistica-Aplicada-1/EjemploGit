347e8bfe648c526bbfb74685a227f6a619f81bfa
CRUR <estadistica.aplicada.itam@gmail.com>
2016-09-04 17:47:27 -0500

    Creacion

---
+++ RendimientoAutos.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+# Utilizamos el data frame mpg (?mpg) y obtenemos informacion sobre dicho objeto.
+cat("Primeras observaciones:\n"); head(mpg)
+cat("Estructura de los datos:\n"); str(mpg)
+cat("Estadisticas descriptivas:\n"); summary(mpg)
